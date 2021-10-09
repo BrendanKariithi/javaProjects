a47347877bf88fffca9d77e1a544a845777d6c73
HGP24 <hgp24@Thes-MacBook-Air-4.local>
2021-10-08 21:11:27 -0700

    Genius Norms

---
+++ GeniusNorms.java
@@ -0,0 +1,41 @@
+
+public class GeniusNorms {
+	public static void main(String[] args) {
+		System.out.println("Hello there, my name is Brendan Kariithi.");//Print My Welcome Message & Name
+		System.out.println("I am 16 years old and my passion is audio fidelity, while my final project is building and designing a loudspeaker"); 
+		System.out.println("");
+		System.out.println("My Brothers Info & Cohort Community Norms"); //Description Of This Program
+		System.out.println("| | | |"); //Marker-Top
+		System.out.println("V V V V"); //Marker-Top
+		System.out.println("");
+		
+		/*Teammate 1 - Omari Pratt */
+		System.out.println("Teammate 1: Omari Pratt."); //Print My Welcome Message & Name
+		System.out.println("14");
+		System.out.println("Arduino Security System");
+		System.out.println("");
+	
+		/*Teammate 2- Yaphet Yonas */
+		System.out.println("Teammate 2: Yaphet Yonas."); //Print My Welcome Message & Name
+		System.out.println("14");
+		System.out.println("Arduino Security System & PHP website");
+		System.out.println("");
+		
+		/*Teammate 3- Isaiah Wilson */
+		System.out.println("Teammate 3: Isaiah Wilson."); //Print My Welcome Message & Name
+		System.out.println("14");
+		System.out.println("Arduino Security System");
+		System.out.println("");
+		
+		/*RICH-5  COMMUNITY NORMS */
+		System.out.println("OUR COMMUNITY NORMS"); //Description 
+		System.out.println("----------------------"); //Divider
+		System.out.println("1. Get Work Done & Work Hard"); //Community Norm 1
+		System.out.println("2. Never give up, Keep going"); //Community Norm 2
+		System.out.println("3. Communicate with Everyone"); //Community Norm 3
+		System.out.println("4. Do your part in Group Assignments"); //Community Norm 4
+		System.out.println("5. Participate Equally in Class"); //Community Norm 5
+		System.out.println("");
+		
+	}
+}
