159e619fd78c98adfea2e3b625b742e5d82c21e2
HGP24 <hgp24@Thes-MacBook-Air-4.local>
2021-11-11 18:12:48 -0800

    adding geniusVariables Class

---
+++ Genius/src/MyGeniusPrograms/geniusVariables.java
@@ -0,0 +1,17 @@
+package MyGeniusPrograms;
+
+public class geniusVariables {
+
+	public static void main(String[] args) {
+		String name = " Brendan Kariithi"; 
+		String timeManagement = " to limit screen time on my phone to four hours a day at most.";
+		String personalCommitment = " focusing on my school work and other commitments before I go and hangout with friends.";
+		String KPIMetrics = " to have a high-level of consumers, increased revenue, and innovative technologies being produced.";
+
+		System.out.println ("Hello my name is" + name + ", the time management strategy that I would like to be held accountable to is" + timeManagement);
+		System.out.println ("I will personally commit to" + personalCommitment);
+		System.out.println ("My business' main KPI's are" + KPIMetrics);
+
+	}
+
+}
