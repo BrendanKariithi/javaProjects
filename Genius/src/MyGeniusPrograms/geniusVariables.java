b7582307e995c4386fb08c8ed564038694b64aab
HGP24 <hgp24@Thes-MacBook-Air-4.local>
2021-11-17 11:32:42 -0800

    updating geniusVariables Class

--- Genius/src/MyGeniusPrograms/geniusVariables.java
+++ Genius/src/MyGeniusPrograms/geniusVariables.java
@@ -3,14 +3,38 @@ package MyGeniusPrograms;
 public class geniusVariables {
 
 	public static void main(String[] args) {
-		String name = " Brendan Kariithi"; 
-		String timeManagement = " to limit screen time on my phone to four hours a day at most.";
-		String personalCommitment = " focusing on my school work and other commitments before I go and hangout with friends.";
-		String KPIMetrics = " to have a high-level of consumers, increased revenue, and innovative technologies being produced.";
-
-		System.out.println ("Hello my name is" + name + ", the time management strategy that I would like to be held accountable to is" + timeManagement);
-		System.out.println ("I will personally commit to" + personalCommitment);
-		System.out.println ("My business' main KPI's are" + KPIMetrics);
+		
+		String name1 = " Brendan Kariithi,"; 
+		String name2 = " Omari Pratt,";
+		String name3 = " Isaiah Wilson,";
+		String name4 = " Yaphet Yonas";
+		
+		String timeManagement1 = " to limit screen time on our phones to four hours a day at most,";
+		String timeManagement2 = " prioritizing assignments,";
+		String timeManagement3 = " focusing on important after school activities before fun activities, and";
+		String timeManagement4 = " following our daily schedules.";
+		
+		
+		String personalCommitment1 = " focusing on school work and other commitments before we go and hangout with friends,";
+		String personalCommitment2 = " managing our ability to focus,";
+		String personalCommitment3 = " focus in school and other important after school activities, and";
+		String personalCommitment4 = " making sure to finish and do everything on our schedules.";
+		
+		String KPIMetrics1 = " to have a high-level of consumers, increased revenue, and innovative technologies being produced,"; 
+		String KPIMetrics2 = " good progress and ready for use,";
+	    String KPIMetrics3 = " how many people are buying the product,";
+	    String KPIMetrics4 = " and the number of people who have bought from or been to my website.";
+	   
+	    System.out.println ("Hello, our names are" + name1 + name2 + name3 + " and" + name4 + ", and the time management strategies that we would like to be held accountable to are" + timeManagement1 + timeManagement2 + timeManagement3 + timeManagement4);
+		System.out.println("");
+	    System.out.println ("We will personally commit to" + personalCommitment1 + personalCommitment2 + personalCommitment3 + personalCommitment4);
+		System.out.println("");
+	    System.out.println ("Our business' main KPI's are" + KPIMetrics1 + KPIMetrics2 + KPIMetrics3 + KPIMetrics4);
+		System.out.println("");
+		
+		System.out.println("We are committing to Holding Each Other Accountable.");
+		System.out.println("");
+		System.out.println("Signed," + name1 + name2 + name3 + name4 + ".");
 
 	}
 
